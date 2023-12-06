d3be2b26bdefe5a1b1bf3ffb0414ff23f294edad
rutvik kikani <rutvikkikani36@gmail.com>
2023-12-06 10:42:09 +0530

    first commit

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+    {
+      _id: 0,
+      name: "$0 to 49",
+      array: [0, 49],
+    },
+    {
+      _id: 1,
+      name: "$50 to 99",
+      array: [50, 99],
+    },
+    {
+      _id: 2,
+      name: "$100 to 149",
+      array: [100, 149],
+    },
+    {
+      _id: 3,
+      name: "$150 to 299",
+      array: [150, 299],
+    },
+    {
+      _id: 4,
+      name: "$300 to 400",
+      array: [300, 400],
+    },
+    {
+      _id: 4,
+      name: "$400 or more",
+      array: [400, 9999],
+    },
+  ];
